d4b2ec7fab9e06e893704e3811b57a9dee4c7f07
Vincent Cataldo <vincentcataldo@me.com>
2020-03-02 14:58:25 -0800

    final commit before submiting assignment

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 function getDogImage(numphotos){
     fetch(`https://dog.ceo/api/breeds/image/random/${numphotos}`)
     .then(response => response.json())
-    .then(responseJson => console.log(responseJson));
+    .then(responseJson =>
+        releasetheHounds(responseJson))
+        .catch(error => alert('Something went wrong. Try again later.'));
 }
 function submitButton(){
 $(".submitform").on("submit", event =>{
@@ -12,8 +14,18 @@ $(".submitform").on("submit", event =>{
     if (numphotos > 50){
        alert("Number needs to be less than 50") 
     }
+    else{
     getDogImage(numphotos);
+}
+$(".dog-results").empty()
 })
 }
+function releasetheHounds(responseJson){
 
+    for (let i=0; i<responseJson.message.length; i++){
+        $('.dog-results').append(
+            `<img src="${responseJson.message[i]}" class="dog-image">`
+        )
+    }
+}
 submitButton();
